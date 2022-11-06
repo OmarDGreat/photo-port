963492dace8c54987fa6afc79df24932ffa7c7a5
OmarD <omar.daghestani@colorado.edu>
2022-11-06 12:12:42 -0700

    added tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,37 +1,71 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
 
+//categories array to check it renders correctly in nav
+const categories = [
+    { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects",},
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
+];
+
+//mock functions, these are taking the place of props for the nav component
+//mocks the prop to see if props are called
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
+//global function from Jest
 afterEach(cleanup);
 
-describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-      render(<Nav />);
+//declares component we're testing
+describe("Nav component", () => {
+    //baseline test
+    it("renders", () => {
+        render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
     });
-  
-    // snapshot test
-    it('matches snapshot', () => {
-      const { asFragment } = render(<Nav />);
-      // assert value comparison
-      expect(asFragment()).toMatchSnapshot();
+    //snapshot test
+    it("matches snapshot", () => {
+        const { asFragment } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
+        expect(asFragment()).toMatchSnapshot();
     });
-  })
+});
 
-  describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    // Arrange
-    const { getByLabelText } = render(<Nav />);
-    // Assert
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-    })
-  })
-
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About me');
+//testing visibility of emoji while testing accessability, the existance of the aria-label
+describe("emoji is visible", () => {
+    it("inserts emoji into the h2", () => {
+        //arrange
+        // we're choosing getByLabelText from testing-library, and bringing it in via desctructuring
+        const { getByLabelText } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
+        //assert
+        expect(getByLabelText("camera")).toHaveTextContent("📸");
     });
-  })
+});
+
+//testing that nav links are visible
+describe("links are visible", () => {
+    it("inserts text into the links", () => {
+        //arrange
+        const { getByTestId } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
+        //assert
+        expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+        expect(getByTestId("about")).toHaveTextContent("About me");
+    });
+});
